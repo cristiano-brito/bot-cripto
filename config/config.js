075facd37b14a6ad10a7a3db5406916f0ae91faa
cristiano-brito <cristianocomputacao@hotmail.com>
2025-02-12 08:16:23 -0300

    Inclusao do arquivo env para variaveis de ambiente, inclusao do arquivo config para a logica de configuracao e calculo de indicadores tecnicos RSI no arquivo index

---
+++ config/config.js
@@ -0,0 +1,18 @@
+// config.js
+
+// Função para obter a URL da API com base no ambiente
+function getApiUrl(env) {
+    const urls = {
+      teste: "https://testnet.binance.vision",
+      producao: "https://api.binance.com"
+    };
+  
+    return urls[env] || urls.producao;
+  }
+  
+  // Definindo o ambiente a partir de uma variável de ambiente ou padrão
+  const environment = process.env.NODE_ENV || "producao";
+  const API_URL = getApiUrl(environment);
+  
+  module.exports = { API_URL, environment };
+  
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 const axios = require("axios");
+const { API_URL, environment } = require('./config/config');
 
 const SYMBOL = 'BTCUSDT'; 
-const PRECO_COMPRA = 34160;
-const PRECO_VENDA = 34501;
-
-const API_URL = "https://testnet.binance.vision"; //https://api.binance.com
+const PERIODO = 14;
 
 let isCompreiBTC = false;
 
@@ -16,26 +16,69 @@ async function venderBTC() {
     console.log("vender");
 }
 
-async function verificarEExecutarOrdemBTC(price) {
-    if (price <= PRECO_COMPRA && isCompreiBTC === false) {
-        await comprarBTC();
+function verificarEExecutarOrdemBTC(rsi) {
+    if (rsi < 30 && isCompreiBTC === false) {
+        console.log('sobrevendido, hora de comprar');
+        comprarBTC();
         isCompreiBTC = true;
-    } else if (price >= PRECO_VENDA && isCompreiBTC === true) {
-        await venderBTC();
+    } else if (rsi > 70 && isCompreiBTC === true) {
+        console.log('sobrecomprado, hora de vender');
+        venderBTC();
         isCompreiBTC = false;
     } else
         console.log('aguardar...');
 }
 
+function medias(prices, periodo, startIndex) {
+    let ganhos = 0, perdas = 0;
+
+    for (let i = 0; i < periodo && (i + startIndex) < prices.length; i++) {
+        const diferenca = prices[i + startIndex] - prices[i + startIndex - 1];
+
+        if(diferenca >= 0)
+            ganhos += diferenca;
+        else
+        perdas += Math.abs(diferenca);
+    }
+    
+    let mediasDeGanhos = ganhos / periodo;
+    let mediasDePerdas = perdas / periodo;
+    return { mediasDeGanhos, mediasDePerdas }
+}
+
+function RSI(prices, periodo) {
+    let mediasDeGanhos = 0, mediasDePerdas = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        let novasMedias = medias(prices, periodo, i);
+        
+        if(i === 1) {
+            mediasDeGanhos = novasMedias.mediasDeGanhos;
+            mediasDePerdas = novasMedias.mediasDePerdas;
+            continue;
+        }
+
+        mediasDeGanhos = (mediasDeGanhos * (periodo - 1) + novasMedias.mediasDeGanhos) / periodo;
+        mediasDePerdas = (mediasDePerdas * (periodo - 1) + novasMedias.mediasDePerdas) / periodo;
+    }
+
+    const rs = mediasDeGanhos / mediasDePerdas;
+    return 100 - (100 / (1 + rs));
+}
+
 async function start() {
-    const {data} = await axios.get(`${API_URL}/api/v3/klines?symbol=${SYMBOL}&interval=1m&limit=21`);
+    const {data} = await axios.get(`${API_URL}/api/v3/klines?symbol=${SYMBOL}&interval=15m&limit=100`);
     const vela = data[data.length - 1];
-    const price = parseFloat(vela[4]);
+    const lastPrice = parseFloat(vela[4]);
 
     console.clear();
-    console.log('Preco: ' + price);
+    console.log('Preco: ' + lastPrice);
 
-    verificarEExecutarOrdemBTC(price);
+    const prices = data.map(k => parseFloat(k[4]));
+    const rsi = RSI(prices, PERIODO);
+    console.log("RSI: " + rsi);
+
+    verificarEExecutarOrdemBTC(rsi);
 }
 
 setInterval(start, 3000);
